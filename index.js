eb165f23a1422dbe9234ef229904c1f524c0d220
Bruno Vasconcelos <brunohgv2@gmail.com>
2023-06-27 23:59:42 -0300

    feat: add items to message history

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello world')
+const form = document.getElementById('form')
+const messageInput = document.getElementById('message')
+const messageHistory = document.getElementById('message-history')
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  const message = document.getElementById('message').value
+
+  addMessageToHistory(message)
+  console.log({message})
+}
+
+function addMessageToHistory(message) {
+  const element = document.createElement('pre')
+  element.innerText = message
+  messageHistory.appendChild(element)
+}

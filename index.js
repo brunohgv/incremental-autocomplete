7557b44e2233e5efbb6a89976145f4b15098c3ff
Bruno Vasconcelos <brunohgv2@gmail.com>
2023-06-28 00:38:41 -0300

    feat: add logic to increment the model once the message is submitted

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const form = document.getElementById('form')
 const messageInput = document.getElementById('message')
 const messageHistory = document.getElementById('message-history')
 
+
 form.onsubmit = (event) => {
   event.preventDefault()
-
+  
   const message = document.getElementById('message').value
-
+  
+  updateTraining(message)
   addMessageToHistory(message)
+
   console.log({message})
 }
 
@@ -15,4 +18,50 @@ function addMessageToHistory(message) {
   const element = document.createElement('pre')
   element.innerText = message
   messageHistory.appendChild(element)
+}
+
+function updateTraining(message) {
+  const memory = readAutocompleteMemory()
+  const words = separateWords(message)
+  const newMemory = updateAutocompleteMemory(memory, words)
+  saveAutocompleteMemory(newMemory)
+}
+
+function separateWords(string) {
+  return string.split(' ').map(el => el.toLowerCase())
+}
+
+function updateAutocompleteMemory(memory, words) {
+  let newMemory = memory
+  words.forEach((word, index) => {
+    if(index === 0) {
+      return
+    }
+    const previousWord = words[index-1]
+    newMemory = incrementPreviousOccurrencyCount(newMemory, previousWord, word)
+  })
+  return newMemory
+}
+
+function incrementPreviousOccurrencyCount(memory, previousWord, word) {
+  if (!memory[previousWord]) {
+    memory[previousWord] = {}
+  }
+  if (memory[previousWord][word]) {
+    memory[previousWord][word] += 1
+  } else {
+    memory[previousWord][word] = 1
+  }
+  return memory
+}
+
+function readAutocompleteMemory() {
+  const fileData = window.localStorage.getItem('autocompleteMemory') || '{}'
+  const jsonData = JSON.parse(fileData)
+  return jsonData
+}
+
+function saveAutocompleteMemory(data) {
+  const stringData = JSON.stringify(data, null, 2)
+  window.localStorage.setItem('autocompleteMemory', stringData)
 }
